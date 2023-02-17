d54d873b40adca892d65b4521ea9378931dde9de
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2023-02-17 08:03:22 +0000

    Update Card.tsx with width

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,10 @@
-import Image from "next/image"
+import Image from "next/image";
 export default function Card() {
-    return <Image alt="dummy placeholder for 640x480" src="https://via.placeholder.com/640x480.png/a59090/000000?Text=640x480" />
+  return (
+    <Image
+      width={640}
+      alt="dummy placeholder for 640x480"
+      src="https://via.placeholder.com/640x480.png/a59090/000000?Text=640x480"
+    />
+  );
 }
