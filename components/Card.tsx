bf816ffdb3231340bf4b4dfe7474ffbfecf1fc57
Soldeplata Saketos <soldeplata.saketos@doclerholding.com>
2023-02-21 15:19:42 +0100

    not use next/image for the POC

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 export default function Card() {
   return (
-    <Image
-      width={640}
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
       alt="dummy placeholder for 640x480"
       src="https://via.placeholder.com/640x480.png/a59090/000000?Text=640x480"
     />
